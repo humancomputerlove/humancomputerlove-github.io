cb779a2fe18c2a84dca84688219cb3dc13de6145
L A R K <lark.buckingham@sjsu.edu>
2019-04-05 00:02:46 -0700

    added background to bubble canvas

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ let bubbleArrayLength;
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent('bgCanvas');
+  canvas.parent('bubbleCanvas');
 
   frameRate(30);
 
@@ -118,3 +118,26 @@ class Bubble {
     }
   }
 }
+
+
+
+let canvasDivInstance = function(p) { // p could be any variable name
+
+  p.canvas;
+  // p.eraseButton = document.querySelector("#eraseButton");
+
+
+  p.setup = function() {
+    p.canvas = p.createCanvas(windowWidth, windowHeight);
+    p.canvas.parent('bgCanvas');
+    // p.background(100, 100, 100);
+  };
+
+  p.draw = function() {
+
+  };
+
+
+}
+
+let canvasDiv = new p5(canvasDivInstance, 'canvasDiv');
