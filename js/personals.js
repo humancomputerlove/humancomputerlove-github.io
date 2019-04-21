0eede34c486d982d6b6b0f414b5d93484e3e8cde
L A R K <lark.buckingham@sjsu.edu>
2019-04-21 15:43:15 -0700

    updated from larkvcr.com working copy

--- js/personals.js
+++ js/personals.js
@@ -12,7 +12,7 @@ let allDataArray;
 let database;
 let keys;
 
-let personalsCreated = false;
+// let personalsCreated = false;
 
 
 // function loadItem(index, filename) {
@@ -62,6 +62,23 @@ function setup() {
     WidthChange(mq);
   }
 
+  // Initialize Firebase
+  let config = {
+    apiKey: "AIzaSyDllsUmTj6d55EqybvIBNSLnM0lvL7Z0aw",
+    authDomain: "humancomputerlove-436b7.firebaseapp.com",
+    databaseURL: "https://humancomputerlove-436b7.firebaseio.com",
+    projectId: "humancomputerlove-436b7",
+    storageBucket: "humancomputerlove-436b7.appspot.com",
+    messagingSenderId: "63005466495"
+  };
+
+  firebase.initializeApp(config);
+
+  database = firebase.database();
+
+
+  let ref = database.ref('personals');
+  ref.on('value', gotData, errData);
 
   // createPersonals();
 
@@ -75,9 +92,9 @@ function draw() {
 
   // console.log(allData);
 
-  if (allData && !personalsCreated) {
-    createPersonals();
-  }
+  // if (allData && !personalsCreated) {
+  //   createPersonals("humanSeeksComputer");
+  // }
 }
 
 
@@ -96,27 +113,47 @@ function WidthChange(mq) {
   // }
 }
 
+function returnToMenu() {
+  document.querySelector('#q1_filter').style.display = 'block';
+  document.querySelector('#personalsDiv').style.display = 'none';
+  document.querySelector('#backButtonDiv').style.display = 'none';
+}
+
+function createPersonals(_type) {
+
+  if (!allData) {
+    alert("Oops! Personals are still loading. Wait a hot sec and try again.")
+  } else {
+    // personalsCreated = true;
+    document.querySelector("#personalsDiv").innerHTML = "";
+    document.querySelector('#q1_filter').style.display = 'none';
+
+    setTimeout(function() {
+      document.querySelector('#backButtonDiv').style.display = 'block';
+      document.querySelector('#personalsDiv').style.display = 'block';
+    }, 0)
 
 
-function createPersonals() {
-  personalsCreated = true;
+    // document.querySelector("#loading").style.visibility = "hidden";
+    document.querySelector("footer").style.display = "block";
 
-  document.querySelector("#loading").style.visibility = "hidden";
+    // shuffle entries
+    for (let i = allDataArray.length - 1; i >= 0; i--) {
+      let randomIndex = Math.floor(Math.random() * (i + 1));
+      let itemAtIndex = allDataArray[randomIndex];
 
-  // shuffle entries
-  for (let i = allDataArray.length - 1; i >= 0; i--) {
-    let randomIndex = Math.floor(Math.random() * (i + 1));
-    let itemAtIndex = allDataArray[randomIndex];
+      allDataArray[randomIndex] = allDataArray[i];
+      allDataArray[i] = itemAtIndex;
+    }
 
-    allDataArray[randomIndex] = allDataArray[i];
-    allDataArray[i] = itemAtIndex;
+    // create post for each entry
+    allDataArray.forEach(function(post) {
+      if (_type === "viewAll") {
+        createCell(post.timeStamp, post.postType, post.postTitle, post.postText, post.contact, post.likes, post.flags);
+      } else if (post.postType === _type)
+        createCell(post.timeStamp, post.postType, post.postTitle, post.postText, post.contact, post.likes, post.flags);
+    });
   }
-
-  // create post for each entry
-  allDataArray.forEach(function(post) {
-    createCell(post.timeStamp, post.postType, post.postTitle, post.postText, post.contact, post.likes, post.flags);
-  });
-
 }
 
 function createCell(_id, _type, _title, _text, _contact, _likes, _flags) {
@@ -137,25 +174,40 @@ function createCell(_id, _type, _title, _text, _contact, _likes, _flags) {
   // console.log(_text.length);
 
   let colSize;
-  // set col size
-  if (_text.length + _title.length * 4 > 400) {
-    colSize = `col-md-5`;
-  } else if (_text.length + _title.length * 4 > 300) {
-    colSize = `col-md-4`;
-  } else {
-    colSize = `col-md-3`;
-  }
+
+  colSize = `col-md-8`;
+
+  // // set col size, varies with text length
+  // if (_text.length + _title.length * 4 > 400) {
+  //   colSize = `col-md-5`;
+  // } else if (_text.length + _title.length * 4 > 300) {
+  //   colSize = `col-md-4`;
+  // } else {
+  //   colSize = `col-md-3`;
+  // }
 
 
+  // // just title
+  // let html = `<h4 class="personalsTitle">${_title}</h4>`;
+
+  // includes body of personal
+  // let html = `<h4 class="personalsTitle">${_title}</h4>
+  //           <p class="personalsText">${_text}</p>
+  //           <p class="contact">${_contact}</p>
+  //           <p class="text-right mb-1"> <a href="javascript:void(0)" onclick="">like</a>
+  //             <a href="javascript:void(0)" onclick="">flag</a><br><span class="likes">${_likes}</span> likes</p>`;
+
+  // includes body of personal
   let html = `<h4 class="personalsTitle">${_title}</h4>
             <p class="personalsText">${_text}</p>
             <p class="contact">${_contact}</p>
-            <p class="text-right mb-1"> <a href="javascript:void(0)" onclick="">like</a>
-              <a href="javascript:void(0)" onclick="">flag</a><br><span class="likes">${_likes}</span> likes</p>`;
+            <p class="text-right mb-1"> <span class="likes">${_likes} </span><a href="javascript:void(0)" onclick="">&#9825;</a>
+            <a href="javascript:void(0)" onclick="">&#9872;</a>
+              </p>`;
 
 
   let post = document.createElement("div");
-  post.className = `${colSize} m-2 h-25 cell ${cellColor} `;
+  post.className = `${colSize} my-2 mx-auto h-25 cell ${cellColor} `;
   post.id = `${_id}`;
   post.innerHTML = html;
 
